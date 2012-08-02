b721ded44cf7c6d17e95766577e284f2c6795140
Gilson Filho <contato@gilsondev.com>
2012-08-02 10:32:07 -0300

    Criando o modelo Speaker

--- src/core/models.py
+++ src/core/models.py
@@ -1,3 +1,17 @@
-from django.db import models
+# -*- coding: utf8 -*-
 
-# Create your models here.
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+
+class Speaker(models.Model):
+    name = models.CharField(_(u'Nome'), max_length=255)
+    slug = models.SlugField(_(u'Slug'))
+    url = models.URLField(_(u'Url'))
+    description = models.TextField(_(u'Descrição'), blank=True)
+    avatar = models.FileField(_(u'Avatar'),
+                              upload_to='palestrantes',
+                              blank=True, null=True)
+
+    def __unicode__(self):
+        return self.name
--- src/core/tests.py
+++ src/core/tests.py
@@ -1,9 +1,28 @@
 # -*- coding: utf8 -*-
 from django.test import TestCase
 
+from .models import Speaker
+
 
 class HomepageTest(TestCase):
     def test_get_homepage(self):
         response = self.client.get('/')
         self.assertEquals(200, response.status_code)
         self.assertTemplateUsed(response, 'index.html')
+
+
+class SpeakerModelTest(TestCase):
+    def setUp(self):
+        self.speaker = Speaker(
+            name="Henrique Bastos",
+            slug="henrique-bastos",
+            url="http://henriquebastos.net",
+            description="Passionate software developer",
+            avatar="")
+        self.speaker.save()
+
+    def test_create(self):
+        self.assertEquals(1, self.speaker.pk)
+
+    def test_unicode(self):
+        self.assertEquals("Henrique Bastos", unicode(self.speaker))
